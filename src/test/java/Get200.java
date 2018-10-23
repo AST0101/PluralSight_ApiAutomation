27985923a4799c097677bbb39e96460119f855fd
Adrian Tamas <tamas.adriansabin@gmail.com>
2018-10-23 11:25:24 +0300

    Removed BASE_ENDPOINT from GET200 class

--- src/test/java/Get200.java
+++ src/test/java/Get200.java
@@ -2,7 +2,6 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,7 +12,6 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class Get200 extends BaseClass {
 
-    public static final String BASE_ENDPOINT = "https://api.github.com";
     CloseableHttpClient client;
     CloseableHttpResponse response;
 
